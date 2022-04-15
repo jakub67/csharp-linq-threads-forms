60fc82dc2215b0c0d5955236e71ab1cd77acf822
jakub67 <94398041+jakub67@users.noreply.github.com>
2022-04-15 12:42:15 +0200

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadSort {
+    static class Program5 {              //SPOUSTET PRES CTRL A F5!!!!!!!
+       
+        private static Form7 formular;            
+        private static Form7 formular2;            
+        private static int[] data3 = { 20, 50, 25, 68, 70, 13, 39, 10, 48, 50, 60, 62, 75, 80, 90 };
+        private static int[] data4 = { 20, 50, 25, 68, 70, 13, 39, 10, 48, 50, 60, 62, 75, 80, 90 };
+        
+        static EventWaitHandle th1Ready = new AutoResetEvent(false); //vl1Ready
+        static EventWaitHandle th2Ready = new AutoResetEvent(false);
+        static EventWaitHandle th1Go = new AutoResetEvent(false);   //vl1Go
+        static EventWaitHandle th2Go = new AutoResetEvent(false);
+        static bool end1 = false, end2 = false;
+        
+        [STAThread]
+        
+        static void Main() {                   //POZOR PROGRAM JEDE POUZE SPUSTENIM POMOCI CTRL FN F5           
+            int[] data = new int[15];
+            int[] data2 = new int[15];
+            var rand = new Random();
+
+            for (int i = 0; i < 15; i++) {
+                data[i] = rand.Next(0, 101);
+                data2[i] = data[i];
+            }
+           
+            formular = new Form7(data3);
+            formular2 = new Form7(data4);
+            formular.StartPosition = FormStartPosition.CenterScreen;
+            formular.Show();
+            formular2.Show();                        //showdialog() ceka na uzavreni formulare, show() ne!!
+            formular.showData(data3, 0, 0);
+            formular2.showData(data4, 0, 0);
+
+            new Thread(bubbleSortSimple).Start(); 
+            new Thread(bubbleSortAdvenced).Start();
+
+
+            while (!end1 || !end2) {            
+                if (!end1) { th1Ready.WaitOne(); }
+                if (!end2) { th2Ready.WaitOne(); }
+                th1Go.Set(); th2Go.Set();
+                Thread.Sleep(1);            
+            }
+            formular.Close();
+            formular2.Close();
+
+        }
+
+        //[Obsolete]
+        private static void bubbleSortSimple() {
+            for (int i = 0; i < data3.Length; i++) {
+                for (int j = 0; j < data3.Length - 1; j++) {                                                         
+                    if (data3[j] > data3[j + 1]) {
+                        int pom = data3[j]; data3[j] = data3[j + 1]; data3[j + 1] = pom;
+                    }
+                    formular.showData(data3, j, j + 1);
+                    th1Ready.Set();
+                    th1Go.WaitOne();
+                    System.Threading.Thread.Sleep(100);                    
+                }
+            }
+            end1 = true;
+        }
+
+        //[Obsolete]
+        private static void bubbleSortAdvenced() {
+            int lastSwapIndex = data4.Length - 1, max = 0;
+            do {
+                max = 0;
+                for (int j = 0; j < lastSwapIndex - 1; j++) {
+                    if (data4[j] > data4[j + 1]) {
+                        int pom = data4[j]; data4[j] = data4[j + 1]; data4[j + 1] = pom; max = j + 1;
+                    }
+                    formular2.showData(data4, j, j + 1);
+                    th2Ready.Set();
+                    th2Go.WaitOne();
+                    System.Threading.Thread.Sleep(100);                   
+                }
+                lastSwapIndex = max;
+            } while (lastSwapIndex > 0);
+            end2 = true;
+        }     
+    }
+}
---
+++ ProgramLogic.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LINQ {
+    public class ProgramLogic {
+        static void Main(string[] args) {
+            PeopleData[] people;
+            var fileContent = File.ReadAllText("people.json");
+            people = JsonSerializer.Deserialize<PeopleData[]>(fileContent);
+            //foreach (PeopleData item in people) {
+            //    Console.WriteLine(item.Last_name);
+            //}
+
+            var orderedByLastName = people.OrderBy(pers => pers.Last_name).ToList();        //co vchazi a podle ceho radit
+            foreach (var item in orderedByLastName) {
+                Console.WriteLine($"{item.Last_name}");
+            }
+            //people.OrderBy(pers => pers.First_name).ToList().ForEach(per => Console.WriteLine(per.First_name));  //BEZ PROMENE VAR!!!
+
+            var orderedByLanguage = people.OrderByDescending(per => per.Language).ToList();
+            foreach (var item in orderedByLastName) {
+                Console.WriteLine($"{item.Language}");
+            }
+            //people.OrderBy(person => person.First_name).ToList().ForEach(person => Console.WriteLine(person.First_name));
+
+            //var ages = people
+            //	.Select(person => new {
+            //		person.Last_name,
+            //		person.Date_of_birth
+            //	})
+            //	.Select(person => $"{person.Last_name} {(DateTime.Today - Convert.ToDateTime(person.Date_of_birth)).Days / 365}")
+            //	.ToList();
+            //foreach (var person in ages) {
+            //	Console.WriteLine(person);
+            //}
+
+            //var ages2 = people
+            //	.Select(person => new {
+            //		person.Last_name,
+            //		Age = (DateTime.Today - Convert.ToDateTime(person.Date_of_birth)).Days / 365
+            //	})
+            //	.Select(personWithDate => $"{personWithDate.Last_name} {personWithDate.Age}")
+            //		.ToList();
+            //foreach (var item in ages2) {
+            //	Console.WriteLine(item);
+            //}
+
+            //GetPeopleOfLang(people);            //nejede .foreach proc???
+
+            var startsWithBe = people.Where(person => person.First_name.StartsWith("Be")).ToList();
+
+            //BEZ VAR JE MOZNY HNED VYPIS!!! JINAK TO NEJEDE, JE TO VOID FUNKCE FOREACH
+            people.Where(person => person.First_name.StartsWith("A")).ToList().ForEach(person => Console.WriteLine(person.First_name));
+
+
+        }
+
+        public static List<PeopleData> GetPeopleOfLang(PeopleData[] people, string language) {
+            return people.Where(person => person.Language == language).ToList();
+        }
+
+        public static object[] GetLAnguages(PeopleData[] people) {
+            return people.Select(person => person.Language).Distinct().OrderBy(lang=>lang).ToArray();
+            //po select jdou dale jen jazyky, order tedy lang=>lang - podle abecedy
+        }
+
+        public static List<PeopleData> LastNameContains(PeopleData[] people, string myString) {
+            return people.Where(person => person.Last_name.Contains(myString)).ToList();
+        }
+    }
+}
